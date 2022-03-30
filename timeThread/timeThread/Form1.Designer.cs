d3f3b3a012e577d99b2ca0d43fb66ab5080c8d36
baariskose <61475377+baariskose@users.noreply.github.com>
2022-03-31 01:29:12 +0300

    Thread

---
+++ timeThread/timeThread/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace timeThread
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(64, 30);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "time";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(906, 490);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1005, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(266, 490);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1005, 537);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(266, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(1316, 30);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(404, 532);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1770, 594);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ timeThread/timeThread/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Timers;
+using System.Diagnostics;
+
+namespace timeThread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.Titles.Add("Time - Thread Line Chart");
+            chart1.ChartAreas[0].AxisX.Interval = 1;
+            chart1.ChartAreas[0].AxisY.Interval = 100;
+        }
+
+        public int[] array = new int[32];
+        struct Param
+        {
+            public int index;
+            public decimal start;
+            public decimal finish;
+        }
+
+        static List<decimal> toplamlar;
+        static decimal sayi = 10000000000; // 10^9
+        static decimal time = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x;
+            int y;
+
+            createTable(dataGridView1);
+
+            new Thread(gauss).Start();
+            for (int a = 1; a <= 32; a++)
+            {
+                List<Thread> threads = new List<Thread>();
+                toplamlar = new List<decimal>();
+
+                Param pr = new Param();
+                decimal temp = sayi / a;
+                pr.start = 1;
+                pr.finish = Math.Floor(temp)  ;
+
+                for (int j = 0; j < a; j++)
+                {
+                    toplamlar.Add(0);
+                    pr.index = j;
+                    Param localPr = pr;
+                    threads.Add(new Thread(new ParameterizedThreadStart(Toplam)));
+                    threads[j].Start(localPr);
+                    pr.start = pr.finish + 1;
+                    pr.finish = pr.start + temp;
+                    pr.finish = pr.finish > sayi ? sayi : pr.finish;
+                }
+
+                foreach (var item in threads)
+                {
+                    item.Join();
+                }
+                decimal toplam = 0;
+
+                foreach (var item in toplamlar)
+                {
+                    toplam = toplam + item;
+                }
+
+                listBox1.Items.Add(a + " iplik icin Sonuc = " + toplam + " Sure = " + time);
+                dataGridView1.Rows[a].Cells[1].Value = time;
+            }
+
+            for (int i = 1; i <= 32; i++)
+            {
+                x = Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value);
+                y = Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value);
+                chart1.Series["time"].Points.AddXY(x, y);
+            }
+        }
+
+        public void createTable(DataGridView table)
+        {
+            table.RowCount = 32;
+            table.ColumnCount = 2;
+            table.Columns[0].Name = "Thread";
+            table.Columns[1].Name = "Time";
+            table.Columns[0].Width = 65;
+            table.Columns[1].Width = 65;
+            int i = table.Rows.Add();
+            for (i = 1; i <= 32; i++)
+            {
+                table.Rows[i].Cells[0].Value = i;
+                table.Rows[i].Cells[1].Value = array[i-1];
+            }
+        }
+
+        static void Toplam(object paramaters)
+        {
+            Param param = (Param)paramaters;
+            decimal toplam = 0;
+            var sw = Stopwatch.StartNew();
+            for (decimal i = param.start; i <= param.finish; i++)
+            {
+                toplam = toplam + i;
+            }
+            sw.Stop();
+            toplamlar[param.index] = toplam;
+            time = sw.ElapsedMilliseconds;
+        }
+
+        static void gauss()
+        {
+            var sw = Stopwatch.StartNew();
+            decimal cevap = sayi / 2;
+            cevap = cevap * (sayi + 1);
+            Console.Write("Gauss Metodu Sonucu = " + cevap + " Sure = ");
+            sw.Stop();
+            Console.Write(sw.ElapsedMilliseconds + "\n");
+        }
+    }
+}
